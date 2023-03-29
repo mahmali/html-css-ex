b1707e16cee2c1cda5e6ec0b2fe3f979f7fc68e1
mahmali <muh.ali.akkaya@gmail.com>
2023-03-29 16:40:09 +0300

    BootstrapVue add

--- html-css/src/App.vue
+++ html-css/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <div id="app">
-
+    <Header></Header>
     <header>
       <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
